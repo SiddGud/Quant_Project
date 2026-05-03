89d6a8ecd08436ad9f3dca392ef78e36221574dd
SiddGud <siddhant.gudwani@students.iiit.ac.in>
2026-05-03 10:55:00 +0530

    implement book update method

--- include/book.hpp
+++ include/book.hpp
@@ -8,7 +8,15 @@ struct OrderBook {
     uint32_t bid_qty   = 0;
     uint32_t ask_qty   = 0;
 
-    bool update(const MarketPacket& pkt) noexcept;
+    [[nodiscard]]
+    bool update(const MarketPacket& pkt) noexcept {
+        bid_price = pkt.bid_price;
+        ask_price = pkt.ask_price;
+        bid_qty   = pkt.bid_qty;
+        ask_qty   = pkt.ask_qty;
+        return true;
+    }
+
     bool has_data() const noexcept { return bid_price > 0; }
     int32_t spread() const noexcept { return ask_price - bid_price; }
 };
